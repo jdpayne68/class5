ba1ae8220ec0da6d83bfe2a818fe217fe198711f
jdpayne68 <jacques.payne@gmail.com>
2025-02-01 22:05:40 -0600

    Update 12-Route53.tf

--- 12-Route53.tf
+++ 12-Route53.tf
@@ -1,19 +1,1 @@
 
-/.data "aws_route53_zone" "main" {
-  name         = "kumo-solutions.com"  # The domain name you want to look up
-  private_zone = false
-}
-
-
-resource "aws_route53_record" "www" {
-  zone_id = data.aws_route53_zone.main.zone_id
-  name    = "kumo-solutions.com"
-  type    = "A"
-
-  alias {
-    name                   = aws_lb.app1_alb.dns_name
-    zone_id                = aws_lb.app1_alb.zone_id
-    evaluate_target_health = true
-  }
-}
-./
